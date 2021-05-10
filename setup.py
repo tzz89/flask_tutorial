3766527513f4c29b2c91b28702d8f87f2fd6fe86
George <teozz89@gmail.com>
2021-05-10 16:09:20 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+# keywords can be found
+# https://docs.python.org/3.7/distutils/setupscript.html#additional-meta-data
+setup(
+    name="src",
+    author="Teo Zuo Zhe",
+    description="Flask tutorial from udemy",
+    version="0.1.0",
+    packages=find_packages
+)
